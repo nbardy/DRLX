3f50bc9318fa9776ce4127b2787320685ef68734
Nicholas Bardy <nicholasbardy@gmail.com>
2023-10-09 02:06:05 -0700

    Update train_luminance.sh

--- train_luminance.sh
+++ train_luminance.sh
@@ -1,4 +1,4 @@
-from drlx.reward_modelling.aesthetics import Aesthetics
+from drlx.reward_modelling.aesthetics import Aesthetics, MaskedLuminance
 from drlx.pipeline.pickapic_prompts import PickAPicPrompts
 from drlx.trainer.ddpo_trainer import DDPOTrainer
 from drlx.configs import DRLXConfig
@@ -9,4 +9,4 @@ pipe = PickAPicPrompts()
 config = DRLXConfig.load_yaml("configs/my_cfg.yml")
 trainer = DDPOTrainer(config)
 
-trainer.train(pipe, Aesthetics())
+trainer.train(pipe, MaskedLuminance())
